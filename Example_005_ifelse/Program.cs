c2d71e7a5716581ee10776fcf31ea3e3271c2eaa
User <1@mail.ru>
2022-08-16 19:10:07 +0300

    Маша

---
+++ Example_005_ifelse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, новенький!");
+}
