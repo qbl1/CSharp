0f096ff205d59f055de6346c454a9bae5c59f08a
User <1@mail.ru>
2022-08-20 09:54:02 +0300

    Add Example_009

--- Example_008_function/Program.cs
+++ Example_008_function/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(result < arg2) result = arg2;
+    if(result < arg3) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 50;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 105;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
