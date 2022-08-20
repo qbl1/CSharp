a18d19eed23039e8708c98332a7580b01ab0743b
User <1@mail.ru>
2022-08-20 10:16:03 +0300

    Ex9

--- Example_009_Array/Program.cs
+++ Example_009_Array/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(result < arg2) result = arg2;
+    if(result < arg3) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 21, 3, 1, 47, 135, 8, 2, 4 };
+array[] 
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
